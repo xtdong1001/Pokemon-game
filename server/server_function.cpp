e9e1afe01fea5f6f5615d8bed9632b10ce617312
xtdong1001 <xtdong1001@163.com>
2019-03-24 18:27:51 -0400

    Add files via upload

--- server/server_function.cpp
+++ server/server_function.cpp
@@ -1,80 +1,54 @@
 #include"server.h"
 
-/*将小精灵信息转化为字符数组保存在a中
-  (用于将小精灵信息同步到服务器)
-  a的格式为{kind,level,exp,}
-  k为小精灵种类  l为小精灵等级 e为小精灵经验*/
 void toString(char*a, elf s)
 {
-    //起始偏移地址
     int count = 0;
 
-    //将小精灵种类转化为字符数组放在a中
     toChar(s.elfKind, a, count);
 
-    //将小精灵等级转化为字符数组放在a中
     toChar(s.level, a, count);
 
-    //将小精灵经验转化为字符数组放在a中
     toChar(s.exp, a, count);
 }
 
-//将用户胜率转化为字符串
 void winRateStr(char*a, int k, int l, int e)
 {
-    //起始偏移地址
     int count = 0;
 
-    //将胜利次数转化为字符数组放在a中
     toChar(k, a, count);
 
-    //将总次数转化为字符数组放在a中
     toChar(l, a, count);
 
     toChar(e, a, count);
 }
 
-//将整数a转化为字符串类型保存在b中  c为字符串起始偏移地址
 void toChar(int a, char*b, int& c)
 {
     char x[10] = { '\0' };
-    //初始化字符数组x
     for (int i = 0; i < 10; i++)
     {
         x[i] = '\0';
     }
-    //调用itoa函数将整型转化为字符数组
     itoa(a, x, 10);
 
-    //将字符数组x复制到b中 起始地址为b[c]
     strncpy(b + c, x, strlen(x));
-    //修改偏移地址 以便下一次存放
     c = c + strlen(x);
 
-    //数字之间用逗号隔开
     b[c] = ',';
     c++;
 }
 
-//从字符数组info中读取小精灵信息，存入kind，level，exp中
-//表示小精灵的种类，等级，经验
-//info格式为{kind,level,exp,}
 void readInfo(char* info, int& kind, int& level, int& exp)
 {
     int i;
 
-    //使用atoi函数将逗号前的字符串转化为整型，得到小精灵种类
     kind = atoi(info);
-    //找到第一个逗号所在的下标（下一次转换的起始偏移地址）
     for (i = 0; info[i] != ','; i++);
     i++;
 
-    //使用atoi函数将逗号前的字符串转化为整型，得到小精灵等级（转换的起始地址为info+i）
     level = atoi(info + i);
 
-    //找到第二个逗号所在的下标（下一次转换的起始偏移地址）
     for (; info[i] != ','; i++);
 
-    //使用atoi函数将逗号前的字符串转化为整型，得到小精灵经验值（转换的起始地址为info+i）
     exp = atoi(info + i + 1);
 }
--- server/thread.cpp
+++ server/thread.cpp
@@ -2,89 +2,76 @@
 
 int main()
 {
-    WSADATA         wsd;            //WSADATA变量  
-    SOCKET          sServer;        //服务器套接字
-    SOCKET          sClient;        //客户端套接字  
-    SOCKADDR_IN     addrServ;;      //服务器地址  
-    int             retVal;         //返回值  
+    WSADATA         wsd;             
+    SOCKET          sServer;        
+    SOCKET          sClient;        
+    SOCKADDR_IN     addrServ;;     
+    int             retVal;       
 
-    //初始化套结字动态库  
     if (WSAStartup(MAKEWORD(2, 2), &wsd) != 0)
     {
         std::cout << "WSAStartup failed!\n";
         return 1;
     }
 
-    //创建套接字  
     sServer = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
-    //创建失败
     if (INVALID_SOCKET == sServer)
     {
         std::cout << "socket failed!\n";
-        WSACleanup();//释放套接字资源;  
+        WSACleanup();
         return  -1;
     }
 
-    //服务器套接字地址   
     addrServ.sin_family = AF_INET;
     addrServ.sin_port = htons(4999);
     addrServ.sin_addr.s_addr = INADDR_ANY;
 
-    //绑定套接字  
     retVal = bind(sServer, (LPSOCKADDR)&addrServ, sizeof(SOCKADDR_IN));
-    //绑定失败
     if (SOCKET_ERROR == retVal)
     {
         std::cout << "bind failed!\n";
-        closesocket(sServer);   //关闭套接字  
-        WSACleanup();           //释放套接字资源;  
+        closesocket(sServer);  
+        WSACleanup();          
         return -1;
     }
 
-    //开始监听   
     retVal = listen(sServer, 1);
     if (SOCKET_ERROR == retVal)
     {
         std::cout << "listen failed!\n";
-        closesocket(sServer);   //关闭套接字  
-        WSACleanup();           //释放套接字资源;  
+        closesocket(sServer);   
+        WSACleanup();          
         return -1;
     }
 
-    //接受客户端请求  
     sockaddr_in addrClient;
     int addrClientlen = sizeof(addrClient);
 
-    //服务器创建多线程
     while (1)
     {
         sClient = SOCKET_ERROR;
-        //等待连接
         while (SOCKET_ERROR == sClient)
         {
             sClient = accept(sServer, (sockaddr FAR*)&addrClient, &addrClientlen);
         }
 
-        //创建线程 一个客户端进程对应一个服务器线程
         DWORD  dwThreadId;
         HANDLE  hThread;
 
         hThread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)server,
             (LPVOID)sClient, 0, &dwThreadId);
-        //创建线程失败
         if (hThread == NULL)
         {
             printf("CreatThread failed.\n");
         }
-        //创建线程成功
         else
         {
             printf("CreateThread  OK.\n");
         }
     }
 
-    closesocket(sServer);   //关闭套接字  
-    WSACleanup();           //释放套接字资源;  
+    closesocket(sServer);    
+    WSACleanup();           
     return 0;
 }
